78178cdea725c43ff579de4a35164345bbd05fea
echobeee <32841554+echobeee@users.noreply.github.com>
2018-02-01 23:26:09 +0800

    Create Node.h

---
+++ Version 3/Node.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Node//huffman树中的每一个节点
+{
+public:
+	Node(){
+		weight = 0;
+		code = "";
+		value = '\0';
+		lfchild = rtchild = parent = -1;
+	}
+	unsigned long weight;
+	string code;
+	unsigned char value;
+	int lfchild, rtchild, parent;
+};
